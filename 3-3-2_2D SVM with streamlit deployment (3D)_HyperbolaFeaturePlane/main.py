bab99178c5387fdf31b5d45b441c2b7f08bc77c1
zhanwei29 <zhanwei929@gmail.com>
2024-11-09 17:25:23 +0800

    Update main.py

--- 3-3-2_2D SVM with streamlit deployment (3D)_HyperbolaFeaturePlane/main.py
+++ 3-3-2_2D SVM with streamlit deployment (3D)_HyperbolaFeaturePlane/main.py
@@ -1,5 +1,5 @@
 #run with command below: 
-#streamlit run "d:/vs code/AIoT_Project/SupportVectorMachine/3-3-2_2D SVM with streamlit deployment (3D)_HyperbolaFeaturePlane/main.py"
+#streamlit run "file_path"
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
